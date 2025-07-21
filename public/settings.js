3173a9045ff8f81046e74e79aa141fce24866be2
mici1708 <118461977+mici1708@users.noreply.github.com>
2025-07-21 19:13:31 +0200

    Update settings.js

--- public/settings.js
+++ public/settings.js
@@ -1,5 +1,9 @@
 function saveSettings() {
-  const username = document.getElementById('username').value;
-  window.Twitch.ext.configuration.set('broadcaster', '1', JSON.stringify({ username }));
-  alert('Username salvato!');
+  if (window.Twitch && window.Twitch.ext) {
+    const username = document.getElementById('username').value;
+    window.Twitch.ext.configuration.set('broadcaster', '1', JSON.stringify({ username }));
+    alert('Username salvato!');
+  } else {
+    alert("Questa pagina deve essere caricata all'interno di Twitch per funzionare.");
+  }
 }
