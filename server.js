7422d468a2b6308b7095a74e66210eed08e50a9c
mici1708 <118461977+mici1708@users.noreply.github.com>
2025-07-21 19:40:10 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 app.post('/get-anilist', async (req, res) => {
   const { username } = req.body;
